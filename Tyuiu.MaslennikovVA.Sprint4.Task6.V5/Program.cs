2743022c6b35c4014e9818a606b2ab928951864c
MaslennikovVladislav <Tyuiu.MaslennikovVA@outlook.com>
2024-12-27 22:31:25 +0500

    Task6

---
+++ Tyuiu.MaslennikovVA.Sprint4.Task6.V5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MaslennikovVA.Sprint4.Task6.V5.Lib;
+
+namespace Tyuiu.MaslennikovVA.Sprint4.Task6.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Масленников В.А. | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Класс Array                                                       *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Масленников Владислав Александрович | ИИПБ-24-2               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан строковый массив данных используя класс Array подсчитайте           *");
+            Console.WriteLine("* количество элементов, длина которых равна 5.                            *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            var planets = new string[] { "Меркурий", "Венера", "Земля", "Марс", "Юпитер", "Сатурн", "Уран", "Нептун" };
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i < planets.Length; i++)
+            {
+                Console.WriteLine(planets[i]);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Колличество элементов длинна которых = 5:");
+
+            int nums = ds.Calculate(planets);
+
+            Console.WriteLine(nums);
+            Console.ReadKey();
+
+        }
+    }
+}
