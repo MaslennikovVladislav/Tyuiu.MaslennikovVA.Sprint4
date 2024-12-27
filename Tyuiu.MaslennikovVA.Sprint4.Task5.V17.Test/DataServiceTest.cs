b392b9a21498ffb39ad85da83d9b221c9722b7fb
MaslennikovVladislav <Tyuiu.MaslennikovVA@outlook.com>
2024-12-27 22:29:14 +0500

    Task5

---
+++ Tyuiu.MaslennikovVA.Sprint4.Task5.V17.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Tyuiu.MaslennikovVA.Sprint4.Task5.V17.Lib;
+
+namespace Tyuiu.MaslennikovVA.Sprint4.Task5.V17.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] mas2 = new int[5, 5] { { -5, 1, -7, 1, 3 },
+                                          { 1, -5, -5, 2, 3 },
+                                          { 3, 1, 3, -8, 2 },
+                                          { 3, 1, 2, -8, 3 },
+                                          { 2, -6, -7, -7, 2 } };
+            int res = ds.Calculate(mas2);
+            int wait = 9;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MaslennikovVA.Sprint4.Task5.V17/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MaslennikovVA.Sprint4.Task5.V17.Lib;
+
+namespace Tyuiu.MaslennikovVA.Sprint4.Task5.V17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт #4 | Выполнил: Масленников В.А. | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы. (генератор случайных чисел)                    *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Масленников Владислав Александрович | ИИПБ-24-2               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* случайными значениями в диапазоне от -9 до 4. Найти количество          *");
+            Console.WriteLine("* отрицательных элементов.                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Дана матрица 5х5");
+            int rows = Convert.ToInt32(5);
+            int columns = Convert.ToInt32(5);
+            int[,] mtrx = new int[rows, columns];
+
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    mtrx[i, j] = rnd.Next(-9, 4);
+                }
+            }
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(mtrx);
+
+            Console.WriteLine("Сумма нечетных чисел = " + res);
+            Console.ReadKey();
+        }
+    }
+}
